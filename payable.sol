91e7c88e4eb53b4091a217558ce11c06f76941f1
Bishal Saha <saha.bishal7676@gmail.com>
2025-05-20 12:23:34 +0530

    Add files via upload

---
+++ payable.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Payable {
+    address payable public owner = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+
+    function payEther() public payable {
+
+    }
+
+    function getBal() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function transferEther() public {
+        owner.transfer(1 ether);
+    }
+}
