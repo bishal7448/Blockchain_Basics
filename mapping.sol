7490c1697f9ad9d3dabc0cf3b23d0758990a8278
Bishal Saha <saha.bishal7676@gmail.com>
2025-05-20 10:52:54 +0530

    Create mapping.sol

---
+++ mapping.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+struct School {
+    string name;
+    uint class;
+    string sec;
+    uint roll;
+}
+…        school[id] = School(name, class, sec, roll);
+    }
+    
+}
